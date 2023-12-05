5ae0a910914128e3ef76d28a3b34016b6f332616
eren595 <eren595@BPC06>
2023-12-05 09:31:25 +0300

    Proje dosyası ekle.

---
+++ NamedParametersSayiIslemleri/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamedParametersSayiIslemleri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SayiIslemleri si= new SayiIslemleri();
+            //int toplam = si.Topla(5,10,15);
+            int toplam = si.Topla(sayi2: 10, sayi3: 15, sayi1: 5);
+            Console.WriteLine("toplam:{0}",toplam);
+        }
+    }
+    class SayiIslemleri
+    {
+        public int Topla(int sayi1, int sayi2, int sayi3)
+        {
+            return sayi1 + sayi2 + sayi3;
+        }
+    }
+}
